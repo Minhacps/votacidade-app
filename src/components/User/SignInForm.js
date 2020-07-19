e8a975a5f7ba1d267d3324a6a250822db0210742
degrecci <henrique.degrecci@upx.com>
2020-07-19 01:53:21 -0300

    adiciona estilos iniciais de background e form

--- src/components/User/SignInForm.js
+++ src/components/User/SignInForm.js
@@ -6,15 +6,45 @@ import {
   FacebookLoginButton,
 } from 'react-social-login-buttons';
 import { TwitterLoginButton } from 'react-social-login-buttons';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import styled from 'styled-components';
+import background from 'assets/img/splashscreen.png';
 
 const buttonStyle = {
   fontSize: '12px',
-  textTransform: 'uppercase',
   marginRight: 0,
   marginLeft: 0,
   boxShadow: 'none',
-  width: 180,
+  width: '100%',
 };
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  margin: auto;
+  justify-content: center;
+`;
+
+const Background = styled.img`
+  min-height: 100%;
+  min-width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const Box = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+`;
+
+const Divider = styled.hr`
+  border-top: 1px solid #6e6e6e;
+`;
 
 const SignInForm = (props) => {
   const [loading, setLoading] = useState(false);
@@ -101,47 +131,61 @@ const SignInForm = (props) => {
   }
 
   return (
-    <div>
-      <h1>Entrar</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input name="email" id="email" data-testid="email-input" />
-
-        <label htmlFor="password">Senha</label>
-        <input
-          name="password"
-          id="password"
-          type="password"
-          data-testid="password-input"
-        />
-
-        <button data-testid="submit-button">Entrar</button>
-      </form>
-
-      <div>
-        <FacebookLoginButton
-          onClick={signInWithFacebook}
-          text="Entrar com Facebook"
-          style={buttonStyle}
-        />
-        <GoogleLoginButton
-          onClick={signInWithGoogle}
-          text="Entrar com Google"
-          style={buttonStyle}
-        />
-        <TwitterLoginButton
-          onClick={signInWithTwitter}
-          text="Entrar com Twitter"
-          style={buttonStyle}
-        />
+    <Container>
+      <Background src={background} />
+      <Box>
+        <h1>Entrar</h1>
         <button
           onClick={() => setShowSignUpForm(true)}
           data-testid="signup-button"
         >
           Cadastrar
         </button>
-      </div>
-    </div>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              name="email"
+              id="email"
+              data-testid="email-input"
+              placeholder="Digite seu e-mail"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label htmlFor="password">Senha</Label>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              data-testid="password-input"
+              placeholder="Digite sua senha"
+            />
+          </FormGroup>
+          <Button data-testid="submit-button" color="primary" block>
+            ENTRAR
+          </Button>
+        </Form>
+        Esqueceu sua senha? <a href="#">Clique aqui</a>
+        <Divider />
+        <div>
+          <FacebookLoginButton
+            onClick={signInWithFacebook}
+            text="Entrar com Facebook"
+            style={buttonStyle}
+          />
+          <GoogleLoginButton
+            onClick={signInWithGoogle}
+            text="Entrar com Google"
+            style={buttonStyle}
+          />
+          <TwitterLoginButton
+            onClick={signInWithTwitter}
+            text="Entrar com Twitter"
+            style={buttonStyle}
+          />
+        </div>
+      </Box>
+    </Container>
   );
 };
 
