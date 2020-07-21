d711b82d7852e0bb2eacff00a65a6c45ffe739a7
degrecci <henrique.degrecci@upx.com>
2020-07-20 23:07:09 -0300

    Adiciona background para desktop

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
 const Background = styled.img`
-  min-height: 100%;
-  min-width: 100%;
+  height: 100%;
   position: fixed;
   top: 0;
   left: 0;
   z-index: -1;
+
+  @media screen and (min-width: 1200px) {
+    width: 100%;
+  }
 `;
 
 export default Background;
