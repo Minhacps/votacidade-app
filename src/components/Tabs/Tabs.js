d5901940e283c153f2d7bc1ec9e153e461685b99
degrecci <henrique.degrecci@upx.com>
2020-07-25 00:44:43 -0300

    Ajusta css de tamanho de tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -10,11 +10,12 @@ const TabButton = styled.button`
   background-color: ${(props) => (props.active ? '#662D91' : '#E6E6E6')};
   color: ${(props) => (props.active ? '#fff' : '#6E6E6E')};
   border: ${(props) => (props.active ? 'none' : '1px solid #C9C9C9')};
-  padding: 9px 23px;
+  padding: 0 20px;
+  height: 30px;
   border-radius: ${(props) =>
     props.active ? '3px 0px 0px 3px' : '0px 3px 3px 0px'};
   text-transform: uppercase;
-  font-size: 10pt;
+  font-size: 10px;
 `;
 
 export { Tabs, TabButton };
