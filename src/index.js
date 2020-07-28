f259176779758b751bffaaa27968c3453746f3e8
Lucas Guima <lucasguima.felipe@gmail.com>
2020-07-28 18:46:13 -0300

    Updated the path to the scss files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './firebase';
 
-import './scss/custom.scss';
+import './assets/style/scss/custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
