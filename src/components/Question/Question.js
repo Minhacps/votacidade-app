9d4826a797b98625383151b54f8941bb42f37280
Victor Miguez <victorcineis@gmail.com>
2020-07-28 23:00:10 +1000

    Implementa funcionalidades para pular, voltar e carregar respostas.
    Co-authored-by: Lucas Guima <lucasguima.felipe@gmail.com>
    Co-authored-by: André Luís Bordignon <albordignon@gmail.com>
    Co-authored-by: João Aleixo  <joaovitordias92040@gmail.com>

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,69 +2,93 @@ import React, { useContext } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { CityContext } from 'components/CityProvider/CityProvider';
 
-const Question = ({ id, onSave }) => {
+const Question = ({ id, onSave, onSkip, onBack, value }) => {
   const { firebase, currentUser, questionnaire } = useContext(CityContext);
   const { question } = questionnaire[id];
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const fields = event.target.elements;
+  const handleChange = (event) => {
+    const { value } = event.target;
 
-    if (!fields.answer.value) {
-      return;
-    }
+    const answer = {
+      [id]: value,
+    };
 
     firebase
       .firestore()
       .collection('answers')
       .doc(currentUser.uid)
-      .set(
-        {
-          [id]: fields.answer.value,
-        },
-        { merge: true },
-      )
-      .then(onSave);
+      .set(answer, { merge: true })
+      .then(() => onSave(answer));
   };
 
   return (
-    <Form onSubmit={handleSubmit} key={id + 1}>
+    <Form onChange={handleChange} key={id + 1}>
       <p>
-        <span>{id + 1}</span>
+        <span>{id + 1}. </span>
         <span>{question}</span>
       </p>
 
       <FormGroup tag="fieldset">
         <FormGroup check className="my-2">
           <Label check>
-            <Input type="radio" id="answer-d" name="answer" value="D" />
+            <Input
+              type="radio"
+              id="answer-d"
+              name="answer"
+              value="D"
+              defaultChecked={value === 'D'}
+            />
             Discordo
           </Label>
         </FormGroup>
 
         <FormGroup check className="my-2">
           <Label check>
-            <Input type="radio" name="answer" value="DP" />
+            <Input
+              type="radio"
+              name="answer"
+              value="DP"
+              defaultChecked={value === 'DP'}
+            />
             Discordo Plenamente
           </Label>
         </FormGroup>
 
         <FormGroup check className="my-2">
           <Label check>
-            <Input type="radio" name="answer" value="C" />
+            <Input
+              type="radio"
+              name="answer"
+              value="C"
+              defaultChecked={value === 'C'}
+            />
             Concordo
           </Label>
         </FormGroup>
 
         <FormGroup check className="my-2">
           <Label check>
-            <Input type="radio" name="answer" value="CP" />
+            <Input
+              type="radio"
+              name="answer"
+              value="CP"
+              defaultChecked={value === 'CP'}
+            />
             Concordo Plenamente
           </Label>
         </FormGroup>
       </FormGroup>
 
-      <Button>Salvar</Button>
+      <div>
+        {id > 0 && (
+          <Button color="primary" outline type="button" onClick={onBack}>
+            Voltar
+          </Button>
+        )}
+        <Button color="primary" outline type="button" onClick={onSkip}>
+          Pular
+        </Button>
+      </div>
     </Form>
   );
 };
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useContext, useState } from 'react';
 import Question from 'components/Question/Question';
 import { CityContext } from 'components/CityProvider/CityProvider';
+import ProgressBar from 'components/ProgressBar/ProgressBar';
 
 const Questions = () => {
-  const { firebase, currentUser } = useContext(CityContext);
+  const { firebase, currentUser, questionnaire } = useContext(CityContext);
   const [isLoading, setIsLoading] = useState(true);
   const [answers, setAnswers] = useState(null);
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -18,21 +19,67 @@ const Questions = () => {
         if (doc.exists) {
           const loadedAnswers = doc.data();
           setAnswers(loadedAnswers);
-          setCurrentQuestion(Number(Object.keys(loadedAnswers).pop() + 1) || 0);
+          setCurrentQuestion(getFirstUnansweredQuestion(loadedAnswers));
         }
         setIsLoading(false);
       });
   }, []);
 
-  const handleSave = () => {
-    setCurrentQuestion(currentQuestion + 1);
+  const getFirstUnansweredQuestion = (loadedAnswers) => {
+    // TODO: handle last question
+
+    const answersKeys = Object.keys(loadedAnswers);
+    const questionsKeys = Object.keys(questionnaire);
+    return Number(
+      questionsKeys.filter(
+        (questionIndex) => !answersKeys.includes(questionIndex),
+      )[0],
+    );
+  };
+
+  const handleNext = (answer) => {
+    const updatedAnswers = {
+      ...answers,
+      ...answer,
+    };
+
+    setAnswers(updatedAnswers);
+
+    if (currentQuestion !== questionnaire.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  const handleBack = () => {
+    setCurrentQuestion(currentQuestion - 1);
+  };
+
+  const getProgress = () => {
+    if (!answers) {
+      return 0;
+    }
+
+    const answersKeys = Object.keys(answers);
+    const questionsKeys = Object.keys(questionnaire);
+    return Number((answersKeys.length / questionsKeys.length) * 100).toFixed(0);
   };
 
   if (isLoading) {
     return null;
   }
 
-  return <Question id={currentQuestion} onSave={handleSave} />;
+  return (
+    <>
+      <ProgressBar progress={getProgress()} />
+      <Question
+        id={currentQuestion}
+        onSave={handleNext}
+        onSkip={handleNext}
+        onBack={handleBack}
+        value={answers && answers[currentQuestion]}
+      />
+    </>
+  );
 };
 
 export default Questions;
