14b450cc8b74b84269a3a267516c7bc7daa25d33
Victor Miguez <victorcineis@gmail.com>
2020-08-07 16:32:38 +1000

    Updates

---
+++ precache-manifest.e67b13a4b4cfe43c62a19a1213a6cd69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5a23e8d3c41c3782c0ee4255681c04",
+    "url": "/votacidade-app/index.html"
+  },
+  {
+    "revision": "ca4c3939068fd41a1fdc",
+    "url": "/votacidade-app/static/css/main.8b76d835.chunk.css"
+  },
+  {
+    "revision": "0589d65b095cd319a9b0",
+    "url": "/votacidade-app/static/js/2.be7fbf39.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/votacidade-app/static/js/2.be7fbf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4c3939068fd41a1fdc",
+    "url": "/votacidade-app/static/js/main.738c329b.chunk.js"
+  },
+  {
+    "revision": "2950e2f6e89fdae3be4b",
+    "url": "/votacidade-app/static/js/runtime-main.36d8a49f.js"
+  },
+  {
+    "revision": "59581140e3c29b3428536dffbeb158e6",
+    "url": "/votacidade-app/static/media/info.59581140.svg"
+  },
+  {
+    "revision": "07c9789faba053f27ab7c38f1467b8a7",
+    "url": "/votacidade-app/static/media/pattern-baixo.07c9789f.png"
+  },
+  {
+    "revision": "2924890541d751e43edaaaa88961c0d1",
+    "url": "/votacidade-app/static/media/splashscreen.29248905.png"
+  }
+]);
