9bef303657648f784d5617ec41552e17892f1732
André Luís Bordignon <albordignon@gmail.com>
2020-08-11 11:00:49 -0300

    Criada página final agradecendo as respostas.
    Co-authored-by: João Aleixo  <joaovitordias92040@gmail.com>

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import Privacidade from '../src/pages/privacidade';
 import cities from './cities';
 import BaseApp from './BaseApp';
 import HomePage from 'pages/Home';
+import FinalPage from 'pages/FinalPage/FinalPage';
 
 const Routes = () => (
   <Switch>
@@ -23,6 +24,14 @@ const Routes = () => (
     <Route path="/privacidade" exact>
       <Privacidade />
     </Route>
+
+    {cities.map((city) => (
+      <Route path={`${city.cityPath}/ranking`} key={`${city.cityPath}-route`}>
+        <CityProvider city={city}>
+          <FinalPage />
+        </CityProvider>
+      </Route>
+    ))}
 
     {cities.map((city) => (
       <Route path={city.cityPath} key={`${city.cityPath}-route`}>
---
+++ src/pages/FinalPage/FinalPage.js
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+import Authenticated from 'templates/Authenticated';
+import styled from 'styled-components';
+import { Container, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { CityContext } from 'components/CityProvider/CityProvider';
+import colors from 'styles/colors';
+
+const StyledUserName = styled.span`
+  color: ${colors.purple};
+  font-weight: 800;
+  font-size: 16pt;
+  margin-bottom: 25px;
+`;
+
+const FinalPage = () => {
+  const { currentUser, cityPath } = useContext(CityContext);
+  const UserName = currentUser.displayName;
+  return (
+    <Authenticated>
+      <Container className="py-4" style={{ lineHeight: '20px' }}>
+        <StyledUserName>Olá, {UserName}!</StyledUserName>
+        <p className="mt-3" style={{ fontSize: '12pt' }}>
+          Obrigado por responder. Em breve a página do índice de afinidade
+          estará no ar. Caso queira mudar alguma resposta do questionário clique
+          no botão abaixo
+        </p>
+        <Button
+          color="primary"
+          className="w-100"
+          style={{ fontWeight: '700' }}
+          to={`${cityPath}/questionario`}
+          tag={Link}
+        >
+          VOLTAR
+        </Button>
+      </Container>
+    </Authenticated>
+  );
+};
+
+export default FinalPage;
