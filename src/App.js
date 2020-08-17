ae18bac1713d24a3bbcbfe646bd704e57be55007
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2020-08-16 22:16:30 -0300

    move logica de autenticacao de App para Routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import firebase from 'firebase/app';
 
 import Routes from './Routes';
-import Login from './pages/Login';
 
-const App = () => {
-  const [lookingForUser, setLookingForUser] = useState(true);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      setLookingForUser(false);
-      setUser(user);
-    });
-  }, []);
-
-  if (lookingForUser) {
-    return <p>Carregando...</p>;
-  }
-
-  if (!user) {
-    return <Login />;
-  }
-
-  return (
-    <Router>
-      <Routes />
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Routes />
+  </Router>
+);
 
 export default App;
