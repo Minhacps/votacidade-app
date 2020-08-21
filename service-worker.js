be9219b1457c11aece8075253a457cb815e02496
victormiguez <victorcineis@gmail.com>
2020-08-21 12:41:06 +0000

    Deploying to gh-pages from  @ 8ac1fa03e3c62e3968ecfbe27737eafe5f293832 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5980a642f749fc27c8395029c3b8f45b.js"
+  "/precache-manifest.1463b92ecbe126e3eae8daaffbce351c.js"
 );
 
 self.addEventListener('message', (event) => {
