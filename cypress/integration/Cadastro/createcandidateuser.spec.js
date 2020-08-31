f95bba71580bb4efbc49787c70c2f5cf910ce7f9
André Luís Bordignon <albordignon@gmail.com>
2020-08-31 16:10:01 -0300

    Teste de candidatos

--- cypress/integration/Cadastro/createcandidateuser.spec.js
+++ cypress/integration/Cadastro/createcandidateuser.spec.js
@@ -1,16 +1,29 @@
-describe('Create candidate user', () => {
-  it('Create candidate user!', () => {
-    cy.visit('http://app.vota.org.br');
-    cy.contains('Cadastrar').click();
+/// <reference types="cypress" />
 
-    cy.get('#name')
-      .type('André Luís Bordignon')
-      .should('have.value', 'André Luís Bordignon');
+import Chance from 'chance';
+const chance = new Chance();
+
+describe('Test', () => {
+  const email = chance.email();
+  const password = 'ifsp@1234';
+  const name = chance.name();
+  const gender = chance.gender();
+  const description = chance.sentence();
+
+  beforeEach(() => {
+    cy.visit('http://app.vota.org.br');
+  });
+
+  it('has a title', () => {
+    cy.contains('votacidade');
+  });
+
+  it('record candidate', () => {
+    cy.contains('Cadastrar').click();
+    cy.get('#name').type(name).should('have.value', name);
     cy.get('#city').select('Campinas');
-    cy.get('#email')
-      .type('albordignon23@gmail.com')
-      .should('have.value', 'albordignon23@gmail.com');
-    cy.get('#password').type('ifsp@1234').should('have.value', 'ifsp@1234');
+    cy.get('#email').type(email).should('have.value', email);
+    cy.get('#password').type(password).should('have.value', password);
     // cy.contains('Sou candidata(o)').check()
     cy.get('[type="checkbox"]').check();
     cy.get('#gender').select('Feminino');
@@ -19,7 +32,17 @@ describe('Create candidate user', () => {
     cy.get('#cnpj').type('49.015.186/0001-41');
     cy.get('#candidateNumber').type(13);
     cy.get('#politicalParty').select('PT');
-    cy.get('#description').type('bla bla bla');
-    cy.contains('Entrar').click();
+    cy.get('#description').type(description);
+    cy.get('button[data-testid=submit-button]').click();
+    cy.contains('Questionário').click();
+
+    for (let i = 0; i < 30; i++) {
+      cy.get('input[value=D]').check();
+      cy.pause();
+      cy.contains('Responder').click();
+    }
   });
+
+  //   cy.contains('Entrar').click();
+  // });
 });
