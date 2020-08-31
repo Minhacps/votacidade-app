901bfdae9d99ac52659cb957ddf299b73308e56e
joaodias <joaovitordias92040@gmail.com>
2020-08-31 16:36:53 -0300

    Colocando conteúdo na Home
    Co-authored-by: André Luís Bordignon <albordignon@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,165 @@
-import React from 'react';
-import Authenticated from 'templates/Authenticated';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { CityContext } from 'components/CityProvider/CityProvider';
+import {
+  Row,
+  Col,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Card,
+  Container,
+  Button,
+} from 'reactstrap';
 
-const HomePage = () => (
-  <Authenticated>
-    <span>home</span>
-  </Authenticated>
-);
+import { Title } from 'UI/Title';
+import colors from 'styles/colors';
+import { Link } from 'react-router-dom';
+
+const StyledCard = styled(Card)`
+  border-radius: 10px;
+  box-shadow: 0px 3px 6px #00000029;
+`;
+
+const StyledUserName = styled.span`
+  color: ${colors.purple};
+  font-weight: 800;
+  font-size: 16pt;
+  margin-bottom: 25px;
+`;
+
+const HomePage = () => {
+  const { currentUser, cityPath } = useContext(CityContext);
+  const UserName = currentUser.displayName;
+
+  return (
+    <Container className="py-4" style={{ lineHeight: '20px' }}>
+      <StyledUserName>Olá, {UserName}!</StyledUserName>
+      <p className="mt-3" style={{ fontSize: '12pt' }}>
+        O Vota Cidade é uma ferramenta para a população encontrar candidatos(as)
+        a vereador(a) que pensam parecido com você nas eleições de 2020. Assim,
+        você escolhe alguém que pensa parecido e pode cobrar melhor depois!
+      </p>
+
+      <Row noGutters>
+        <Col
+          md="6"
+          xl="3"
+          className="pr-md-2 pr-lg-3 pr-xl-2 mb-4 d-flex align-items-stretch"
+        >
+          <StyledCard>
+            <CardImg
+              top
+              width="100%"
+              src={require('assets/img/cards/como-funciona.png')}
+              alt="Como funciona"
+            />
+            <CardBody>
+              <CardTitle>
+                <Title size="20px" color={colors.purple}>
+                  Como funciona?
+                </Title>
+              </CardTitle>
+              <CardText>
+                Você responderá um questionário de 30 perguntas, com questões
+                variadas sobre políticas públicas como transporte, saneamento
+                básico, drogas, etc.
+              </CardText>
+            </CardBody>
+          </StyledCard>
+        </Col>
+        <Col
+          md="6"
+          xl="3"
+          className="pl-md-2 pl-lg-3 px-xl-2 mb-4 d-flex align-items-stretch"
+        >
+          <StyledCard>
+            <CardImg
+              top
+              width="100%"
+              src={require('assets/img/cards/afinidade.png')}
+              alt="Afinidade"
+            />
+            <CardBody>
+              <CardTitle>
+                <Title size="20px" color={colors.purple}>
+                  Afinidade
+                </Title>
+              </CardTitle>
+              <CardText>
+                Nós cruzaremos as suas respostas com os candidatos e candidatas
+                cadastradas e te mostraremos quem pensa parecido com você!
+              </CardText>
+            </CardBody>
+          </StyledCard>
+        </Col>
+        <Col
+          md="6"
+          xl="3"
+          className="pr-md-2 pr-lg-3 px-xl-2 mb-4 d-flex align-items-stretch"
+        >
+          <StyledCard>
+            <CardImg
+              top
+              width="100%"
+              src={require('assets/img/cards/quero-mais.png')}
+              alt="Quero mais"
+            />
+            <CardBody>
+              <CardTitle>
+                <Title size="20px" color={colors.purple}>
+                  Quero mais!
+                </Title>
+              </CardTitle>
+              <CardText>
+                Você poderá pesquisar e acessar o perfil de qualquer candidato
+                ou candidata cadastrada para ver as respostas, comparar e tomar
+                a sua decisão.
+              </CardText>
+            </CardBody>
+          </StyledCard>
+        </Col>
+        <Col
+          md="6"
+          xl="3"
+          className="pl-md-2 pl-lg-3 pl-xl-2 mb-4 d-flex align-items-stretch"
+        >
+          <StyledCard>
+            <CardImg
+              top
+              width="100%"
+              src={require('assets/img/cards/seguranca-dos-dados.png')}
+              alt="Segurança dos Dados"
+            />
+            <CardBody>
+              <CardTitle>
+                <Title size="20px" color={colors.purple}>
+                  E os dados?
+                </Title>
+              </CardTitle>
+              <CardText>
+                Nós nos preocupamos e respeitamos muito seus dados e por isso
+                daremos a opção de responder o questionário de forma anônima.
+              </CardText>
+            </CardBody>
+          </StyledCard>
+        </Col>
+      </Row>
+
+      <Link to={`${cityPath}/questionario`}>
+        <Button
+          color="primary"
+          type="button"
+          className="w-100"
+          style={{ fontWeight: '700' }}
+          onClick="/questionario"
+        >
+          COMEÇAR
+        </Button>
+      </Link>
+    </Container>
+  );
+};
 
 export default HomePage;
