585377ea45d188c67c0b1c1d3e6761b294fccdbc
André Luís Bordignon <albordignon@gmail.com>
2020-09-07 09:58:40 -0300

    Corrigindo conflitos da branch questions-board e header-city-name.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'reactstrap';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+
+import { CityContext } from 'components/CityProvider/CityProvider';
 
 import logoVota from 'assets/img/logos/vota.svg';
-import logo from 'assets/img/logos/vota.svg';
 import logoVotaCidade from 'assets/img/logos/logo-vota-cidades.svg';
 
 export const Header = () => {
+  const { cityName } = useContext(CityContext);
+
   return (
     <header>
-      <Navbar className="col" light expand="md">
-        <Link to="/">
-          <img src={logo} alt="Logo VotaCidades" />
-        </Link>
+      <Navbar light expand="md">
+        <Container>
+          <NavbarBrand tag={Link} to="/" title="Vota Cidade">
+            <img
+              src={logoVota}
+              alt="Logo Vota Cidade"
+              style={{ height: '49px' }}
+              className="d-block d-sm-none"
+            />
+            <img
+              src={logoVotaCidade}
+              alt="Logo Vota Cidade"
+              className="d-none d-sm-block"
+            />
+          </NavbarBrand>
+          <NavbarBrand
+            tag={Link}
+            to="/"
+            title={cityName}
+            className="text-muted font-weight-bold"
+            style={{ margin: 'auto' }}
+          >
+            {cityName}
+          </NavbarBrand>
+        </Container>
       </Navbar>
     </header>
   );
