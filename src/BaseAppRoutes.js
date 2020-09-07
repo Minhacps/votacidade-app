73872894608233ae8df2ebcec486e22b5aef5be2
joaodias <joaovitordias92040@gmail.com>
2020-09-07 16:46:16 -0300

    Inserindo FinalPage no BaseAppRoutes

--- src/BaseAppRoutes.js
+++ src/BaseAppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Questions from 'components/Questions/Questions';
 import HomePage from 'pages/Home';
+import FinalPage from 'pages/FinalPage/FinalPage';
 
 const BaseAppRoutes = ({ cityPath, user }) => {
   return (
@@ -11,6 +12,9 @@ const BaseAppRoutes = ({ cityPath, user }) => {
       </Route>
       <Route path={`${cityPath}/questionario`} exact>
         <Questions user={user} />
+      </Route>
+      <Route path={`${cityPath}/ranking`} exact>
+        <FinalPage />
       </Route>
     </Switch>
   );
