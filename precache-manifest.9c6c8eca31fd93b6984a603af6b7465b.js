510bb55ec24aae88e1ea922de62ff0f891234eff
victormiguez <victorcineis@gmail.com>
2020-09-08 12:51:10 +0000

    Deploying to gh-pages from  @ 8cc114edb028efa74dc7d6b9b9b5efcca10c36c8 🚀

---
+++ precache-manifest.9c6c8eca31fd93b6984a603af6b7465b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cfb59b38d1eb9aba9b93d8d36cb68e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a255fe18efab1f8ea5a",
+    "url": "/static/css/main.8b76d835.chunk.css"
+  },
+  {
+    "revision": "de1f63e025f9e2415f76",
+    "url": "/static/js/2.27b6afa9.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.27b6afa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a255fe18efab1f8ea5a",
+    "url": "/static/js/main.45427567.chunk.js"
+  },
+  {
+    "revision": "25d4e3f37281faf3cecb",
+    "url": "/static/js/runtime-main.f8a93575.js"
+  },
+  {
+    "revision": "957a493e56c8de1fb536d9b7a6f025ca",
+    "url": "/static/media/afinidade.957a493e.png"
+  },
+  {
+    "revision": "32947da0f32efe13c8fe9b2b098f534e",
+    "url": "/static/media/campina-grande-fundo.32947da0.jpg"
+  },
+  {
+    "revision": "e45bfdc1c5785048107f9c188061f2ee",
+    "url": "/static/media/campinas-fundo.e45bfdc1.jpg"
+  },
+  {
+    "revision": "8dab8e7a2dc83cdedd04c7132404dc43",
+    "url": "/static/media/jampa-fundo.8dab8e7a.png"
+  },
+  {
+    "revision": "b37ef5cdc55a706d010de252799aded1",
+    "url": "/static/media/logo-front-campinas.b37ef5cd.svg"
+  },
+  {
+    "revision": "0f2f803e4cfef1b3ab14fb9050946d47",
+    "url": "/static/media/logo-minha-jampa.0f2f803e.png"
+  },
+  {
+    "revision": "b27c7f56c40ac070d84250a51c6b7375",
+    "url": "/static/media/logo-vota-cidades.b27c7f56.svg"
+  },
+  {
+    "revision": "35c8ab70f20f0191fd173c7ec382f9d8",
+    "url": "/static/media/pattern-baixo.35c8ab70.png"
+  },
+  {
+    "revision": "061f98da1ba15f0f110d28b859833251",
+    "url": "/static/media/poa-fundo.061f98da.jpg"
+  },
+  {
+    "revision": "84310a14563d330d94b2526eb399eef4",
+    "url": "/static/media/recife-fundo.84310a14.jpg"
+  },
+  {
+    "revision": "047a981f4243342048f21f5a6d93cdde",
+    "url": "/static/media/splashscreen.047a981f.png"
+  },
+  {
+    "revision": "99573ed2616c402f0a7ef88d7b606b9e",
+    "url": "/static/media/vota.99573ed2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10b38c9b0b967ce6464081d59a87e3e0.js"
+  "/precache-manifest.9c6c8eca31fd93b6984a603af6b7465b.js"
 );
 
 self.addEventListener('message', (event) => {
