3f12d1ee562d9aceaa74d95f65e6957800e1d014
André Luís Bordignon <albordignon@gmail.com>
2020-09-11 08:05:31 -0300

    Ajustadas as questões de João Pessoa, Porto Alegre e Recife.

--- src/cities/portoAlegre.js
+++ src/cities/portoAlegre.js
@@ -10,12 +10,7 @@ const questionnaire = [
   },
   {
     question:
-      'A privatização da gestão de água e esgoto vai facilitar o acesso à água em bairros mais pobres.	',
-    explanation: '',
-  },
-  {
-    question:
-      'Os 4.700 leitos SUS da capital são insuficientes para atender à demanda por internações de pacientes de Porto Alegre e do interior do Estado do RS.',
+      'A privatização da gestão de água e esgoto vai facilitar o acesso à água em bairros mais pobres.',
     explanation: '',
   },
   {
@@ -25,140 +20,143 @@ const questionnaire = [
   },
   {
     question:
-      'Cotas sociais geram ainda mais discriminação, pois tratam os cotistas como menos capazes.	',
+      'Taxar o uso do carro é uma boa solução para incentivar o uso do transporte coletivo.',
     explanation: '',
   },
   {
     question:
-      'Taxar o uso do carro é uma boa solução para incentivar o uso do transporte coletivo.	',
+      'Os(as) candidatos(as) eleitos(as) podem tomar decisões de interesse público sem ouvir a população, uma vez que são representantes do povo.',
     explanation: '',
   },
   {
     question:
-      'O preço da passagem de ônibus em Porto Alegre deve ser estabelecido de forma que traga lucro para as prestadoras do serviço.	',
+      'O mais importante é que as pessoas façam o isolamento social para evitar a disseminação do coronavírus enquanto não temos vacina.',
+    explanation: '',
+  },
+  {
+    question: 'As instituições religiosas devem continuar sem pagar impostos.',
     explanation: '',
   },
   {
     question:
-      'Os(as) candidatos(as) eleitos(as) podem tomar decisões de interesse público, como projetos de lei, sem ouvir a população, uma vez que são representantes do povo.	',
+      'O Brasil deve cobrar menos impostos de grandes fortunas e heranças.',
     explanation: '',
   },
   {
     question:
-      'O Orçamento Participativo é apenas uma fachada pois na realidade a população não participa das decisões orçamentárias de Porto Alegre.	',
+      'A ação da polícia deve ser protegida, livre de acusações de abuso de autoridade.',
+    explanation: '',
+  },
+  {
+    question:
+      'O governo deve fornecer auxílio econômico e psicológico a mulheres que saem de casa por causa da violência doméstica.',
+    explanation: '',
+  },
+  {
+    question:
+      'Os 4.700 leitos SUS da capital são insuficientes para atender à demanda por internações de pacientes de Porto Alegre e do interior do Estado do RS.',
+    explanation: '',
+  },
+  {
+    question:
+      'Cotas sociais geram ainda mais discriminação, pois tratam os cotistas como menos capazes.',
+    explanation: '',
+  },
+  {
+    question:
+      'O preço da passagem de ônibus em Porto Alegre deve ser estabelecido de forma que traga lucro para as prestadoras do serviço.',
+    explanation: '',
+  },
+  {
+    question:
+      'O Orçamento Participativo é apenas uma fachada pois, na realidade, a população não participa das decisões orçamentárias de Porto Alegre.',
     explanation:
-      'O Orçamento Participativo é um ferramenta de participação do cidadão nas decisões sobre os orçamentos públicos, como por exemplo, em obras de infraestrutura e saneamento. Ele ocorre por meio de assembleias abertas e periódicas.	',
+      'O Orçamento Participativo é um ferramenta de participação do cidadão nas decisões sobre os orçamentos públicos, como por exemplo, em obras de infraestrutura e saneamento. Ele ocorre por meio de assembleias abertas e periódicas.',
   },
   {
     question:
-      'O mais importante é que as pessoas façam o isolamento social para evitar a disseminação do coronavírus enquanto não temos vacina.	',
-    explanation: '',
-  },
-  {
-    question:
-      'Os leitos de UTI criados para internações por coronavírus estão de acordo com a demanda da capital para tratar pacientes com quadro grave de infecção.	',
+      'Os leitos de UTI criados para internações por coronavírus estão de acordo com a demanda da capital para tratar pacientes com quadro grave de infecção.',
     explanation:
-      'Até o final de junho, haviam sido criados cerca de 118 leitos de UTI em Porto Alegre. Mais 30 vagas estão previstas para serem criadas em breve, o que totalizaria 148 novos leitos de UTI.	',
+      'Até o final de junho, haviam sido criados cerca de 118 leitos de UTI em Porto Alegre. Mais 30 vagas estão previstas para serem criadas em breve, o que totalizaria 148 novos leitos de UTI.',
   },
   {
-    question: 'As instituições religiosas devem continuar sem pagar impostos.	',
-    explanation: '',
-  },
-  {
-    question:
-      'O Brasil deve cobrar menos impostos de grandes fortunas e heranças.	',
-    explanation: '',
-  },
-  {
-    question: 'A renda básica deve ser implementada como política pública.	',
+    question: 'A renda básica deve ser implementada como política pública.',
     explanation:
-      'A Renda Básica de Cidadania propõe uma renda para todo(a) cidadã(o) no Brasil, independente de qualquer coisa	',
+      'O projeto Renda Básica de Cidadania propõe uma renda para todo(a) cidadão(ã) no Brasil. A ideia é que, com essa verba, não faltem condições básicas de sobrevivência, como alimento e abrigo, a ninguém.',
   },
   {
     question:
-      'A ação da polícia deve ser protegida, livre de acusações de abuso de autoridade.	',
+      'A Polícia usa de violência descabida ao reprimir atos políticos nas ruas da capital.',
     explanation: '',
   },
   {
     question:
-      'A Polícia usa de violência descabida ao reprimir atos políticos nas ruas da capital.	',
-    explanation: '',
+      'A tipificação de homícidios como "feminicídios" é fundamental para um melhor enfrentamento deste crime.", explanation: "Feminicídio é como se tipifica o assassinatos de mulheres cometidos em razão do gênero, isto é, quando a vítima é morta por ser mulher. Esse tipo de crime tem uma pena maior que os demais assassinatos.	',
   },
   {
     question:
-      'O governo deve fornecer auxílio econômico e psicológico a mulheres que saem de casa por causa da violência doméstica.	',
-    explanation: '',
-  },
-  {
-    question:
-      "A tipificação de homícidios como 'feminicídios' é fundamental para um melhor enfrentamento deste crime.	",
+      'A Lei da Meia-entrada de Porto Alegre que beneficia, por exemplo, jovens de baixa renda, promove efetivamente o acesso à cultura.',
     explanation:
-      'Feminicídio é como se tipifica o assassinatos de mulheres cometidos em razão do gênero, isto é, quando a vítima é morta por ser mulher. Esse tipo de crime tem uma pena maior que os demais assassinatos.	',
+      'A Lei da Meia-entrada dá direito a estudantes, idosos, jovens de baixa renda, doadores de sangue regulares e pessoas com deficiência pagarem metade do valor do ingresso em eventos culturais.',
   },
   {
     question:
-      'A Lei da Meia-entrada de Porto Alegre, que beneficia, por exemplo, jovens de baixa renda e doadores de sangue, promove efetivamente o acesso à cultura.	',
-    explanation:
-      'A Lei da Meia-entrada dá direito a estudantes, idosos, jovens de baixa renda e pessoas com deficiência pagarem metade do valor do ingresso em eventos culturais.	',
-  },
-  {
-    question:
-      'A proibição da circulação dos carrinhos de catadores e catadoras contribui para uma cidade mais limpa.	',
+      'A proibição da circulação dos carrinhos de catadores e catadoras contribui para uma cidade mais limpa.',
     explanation: '',
   },
   {
     question:
-      'Os danos ambientais gerados pela construção da Mina Guaíba são aceitáveis se pensarmos na geração de renda e empregos que o empreendimento trará para a região.	',
+      'Os danos ambientais gerados pela construção da Mina Guaíba são aceitáveis se pensarmos na geração de renda e empregos que o empreendimento trará para a região.',
     explanation:
-      'A Mina Guaíba é um projeto da Copelmi Mineração para construir uma mina de cavão a céu aberto, em uma área de quatro mil hectares nos municípios de Eldorado do Sul e Charqueadas.	',
+      'A Mina Guaíba é um projeto da Copelmi Mineração para construir uma mina de cavão a céu aberto, em uma área de quatro mil hectares nos municípios de Eldorado do Sul e Charqueadas.',
   },
   {
-    question: 'O uso da maconha deve ser discriminalizado.	',
+    question: 'O uso da maconha deve ser discriminalizado.',
     explanation: '',
   },
   {
     question:
-      'A disseminação de notícias falsas não é um risco para a democracia.	',
+      'A disseminação de notícias falsas não é um risco para a democracia.',
     explanation: '',
   },
   {
     question:
-      'Após a Prefeitura exigir a reforma de calçadas na área central, o trânsito para cadeirantes foi bastante facilitado.	',
+      'Após a Prefeitura exigir a reforma de calçadas na área central, o trânsito para cadeirantes foi bastante facilitado.',
     explanation:
-      'O Programa Minha Calçada, da Secretaria de Municipal de Obras e Viação, lançado em 2011, busca a revitalização das calçadas da cidade além da conscientização dos cidadãos sobre as responsabilidades de conservação dos passeios públicos.	',
+      'O Programa Minha Calçada, da Secretaria de Municipal de Obras e Viação, lançado em 2011, busca a revitalização das calçadas da cidade além da conscientização dos cidadãos sobre as responsabilidades de conservação dos passeios públicos.',
   },
   {
     question:
-      'A forma como a história da África e a construção da identidade negra é contada nas escolas ajuda a perpetuar o preconceito e o racismo.	',
+      'A forma como a história da África e a construção da identidade negra é contada nas escolas ajuda a perpetuar o preconceito e o racismo.',
     explanation: '',
   },
   {
     question:
-      'Discussões sobre gênero e sexualidade não devem ocorrer em ambiente escolar.	',
+      'Discussões sobre gênero e sexualidade não devem ocorrer em ambiente escolar.',
     explanation: '',
   },
   {
     question:
-      'Políticas públicas de acolhimento aos imigrantes também promovem o desenvolvimento do Estado.	',
+      'Políticas públicas de acolhimento aos imigrantes também promovem o desenvolvimento do Estado.',
     explanation:
-      'Exemplos de ações de acolhimento: oferecer aulas de português, mediar a inserção no mercado de trabalho, apoiar a regularizar de documentação, fornecer vale-alimentação para deslocamento, entre outras.	',
+      'Exemplos de ações de acolhimento: oferecer aulas de português, mediar a inserção no mercado de trabalho, apoiar a regularizar de documentação, fornecer vale-alimentação para deslocamento, entre outras.',
   },
   {
     question:
-      'A população em situação de rua opta por ficar na rua, mesmo com a oferta de vagas em albergues pela Prefeitura.	',
+      'A população em situação de rua opta por ficar na rua mesmo com a oferta de vagas em albergues pela Prefeitura.',
     explanation:
-      'Na Capital, são 100 vagas em albergues, onde as pessoas podem passar a noite, e 495 vagas em abrigos, onde há ações de assistência mas não há estrutura para a pernoite.	',
+      'Na Capital, estão disponíveis 100 vagas em albergues onde as pessoas podem passar a noite e 495 vagas em abrigos onde há ações de assistência mas não há estrutura para a pernoite.',
   },
   {
     question:
-      'O cercamento e a privatização de praças e parques é a melhor alternativa para a melhora nos cuidados destes espaços.	',
+      'O cercamento e a privatização de praças e parques é a melhor alternativa para a melhora nos cuidados destes espaços.',
     explanation: '',
   },
   {
     question:
-      'O programa Moradia Primeiro é ineficiente, pois as pessoas abrigadas não conseguem ser plenamente reintegradas à sociedade.	',
+      'O programa Moradia Primeiro é ineficiente, pois as pessoas abrigadas não conseguem ser plenamente reintegradas à sociedade.',
     explanation:
-      'Moradia Primeiro é um programa de aluguel solidário da prefeitura, em que pessoas em situação de rua podem morar por 6 meses em um local alugado pela prefeitura e pago diretamente ao dono do imóvel.	',
+      'Moradia Primeiro é um programa de aluguel solidário da prefeitura, em que pessoas em situação de rua podem morar por 6 meses em um local alugado pela prefeitura e pago diretamente ao dono do imóvel.',
   },
 ];
 
