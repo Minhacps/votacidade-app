f64ee5fd3705617ef010bb135d79e6909d15497d
Lucas Guima <lucasguima.felipe@gmail.com>
2020-09-19 11:06:44 -0300

    Add the state (UF) to the cities

--- src/data/cidades.js
+++ src/data/cidades.js
@@ -1,32 +1,37 @@
 export const cidades = [
   {
     title: 'Campinas',
+    state: 'SP',
     image: require('../../src/assets/img/photos_city/campinas-fundo.jpg'),
     linkTo: 'https://www.eufaco.minhacampinas.org.br',
     value: 'campinas',
   },
   {
     title: 'João Pessoa',
+    state: 'PB',
     image: require('../../src/assets/img/photos_city/jampa-fundo.png'),
     linkTo: 'https://www.minhajampa.org.br/#block-14338',
     value: 'joao-pessoa',
   },
   {
     title: 'Porto Alegre',
+    state: 'RS',
     image: require('../../src/assets/img/photos_city/poa-fundo.jpg'),
     linkTo: 'https://www.minhaportoalegre.org.br/#block-12312',
     value: 'porto-alegre',
   },
   {
     title: 'Recife',
+    state: 'PE',
     image: require('../../src/assets/img/photos_city/recife-fundo.jpg'),
     linkTo: 'https://www.meurecife.org.br/#block-4456',
     value: 'recife',
   },
-  // {
-  //   title: 'Campina Grande',
-  //   image: require('../../src/assets/img/photos_city/campina-grande-fundo.jpg'),
-  //   linkTo: 'https://www.facebook.com/MinhaCampinaRNC/',
-  //   value: 'campina-grande',
-  // },
+  /*{
+    title: 'Campina Grande',
+    state: 'PB',
+    image: require('../../src/assets/img/photos_city/campina-grande-fundo.jpg'),
+    linkTo: 'https://www.facebook.com/MinhaCampinaRNC/',
+    value: 'campina-grande',
+  },*/
 ];
