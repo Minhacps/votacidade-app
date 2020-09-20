b5658720f372676f676c9075b3ad78102c95f254
Lucas Guima <lucasguima.felipe@gmail.com>
2020-09-20 18:16:42 -0300

    Place each button inside a Col
    Use the block prop to set the button full width

--- src/components/molecules/QuestionnaireActions.jsx
+++ src/components/molecules/QuestionnaireActions.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import { Button, FormGroup } from 'reactstrap';
+import { Button, Col, FormGroup } from 'reactstrap';
 
 import { ROLE_CANDIDATE } from 'constants/userRoles';
 
-const QuestionnaireAction = ({ userRole, questionnaireLength, questionIndex, onSkip, onBack }) => (
-  <FormGroup>
+const QuestionnaireAction = ({ userRole, questionnaireLength, answersLength, questionIndex, onBack, onSkip }) => (
+  <FormGroup row>
     {questionIndex > 0 && (
-      <Button
-        color="primary"
-        outline
-        type="button"
-        onClick={onBack}
-        className="w-100 mr-4"
-      >
-        Anterior
-      </Button>
+      <Col>
+        <Button
+          color="primary"
+          outline
+          block
+          type="button"
+          onClick={onBack}
+        >
+          Anterior
+        </Button>
+      </Col>
     )}
 
     {questionIndex < questionnaireLength - 1 && (
-      <Button
-        color="primary"
-        outline
-        type="button"
-        onClick={() => onSkip()}
-        className="w-100 mr-4"
-      >
-        {userRole === ROLE_CANDIDATE ? 'Pular' : 'Próxima'}
-      </Button>
+      <Col>
+        <Button
+          color="primary"
+          outline
+          block
+          type="button"
+          onClick={() => onSkip()}
+        >
+          {userRole === ROLE_CANDIDATE ? 'Pular' : 'Próxima'}
+        </Button>
+      </Col>
     )}
 
     {userRole === ROLE_CANDIDATE && (
-      <Button type="submit" color="primary" className="w-100" outline>
-        {questionIndex === questionnaireLength - 1 ? 'Finalizar' : 'Responder'}
-      </Button>
+      <Col>
+        <Button type="submit" color="primary" block>
+          {questionIndex === questionnaireLength - 1 ? 'Finalizar' : 'Responder'}
+        </Button>
+      </Col>
     )}
   </FormGroup>
 );
