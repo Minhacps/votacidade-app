a9b257b9cee637a943f21a8df8de8e02e2bd7318
Lucas Guima <lucasguima.felipe@gmail.com>
2020-09-20 21:07:00 -0300

    Show the Finalizar button to voters when they reach the minimum of answers

--- src/components/molecules/QuestionnaireActions.jsx
+++ src/components/molecules/QuestionnaireActions.jsx
@@ -1,48 +1,44 @@
 import React from 'react';
 import { Button, Col, FormGroup } from 'reactstrap';
 
-import { ROLE_CANDIDATE } from 'constants/userRoles';
+import { ROLE_CANDIDATE, ROLE_VOTER } from 'constants/userRoles';
 
-const QuestionnaireAction = ({ userRole, questionnaireLength, answersLength, questionIndex, onBack, onSkip }) => {
-  const lastQuestionIndex = questionnaireLength - 1;
+const QuestionnaireAction = ({ userRole, questionnaireLength, answersLength, minAnswers, questionIndex, alreadyAnswered, onBack, onSkip }) => (
+  <FormGroup row>
+    <Col>
+      <Button
+        color="primary"
+        outline
+        block
+        type="button"
+        onClick={onBack}
+        disabled={questionIndex === 0}
+      >
+        Anterior
+      </Button>
+    </Col>
 
-  return (
-    <FormGroup row>
+    <Col>
+      <Button
+        color="primary"
+        outline
+        block
+        type="button"
+        onClick={() => onSkip()}
+        disabled={questionIndex === questionnaireLength - 1}
+      >
+        {userRole === ROLE_CANDIDATE ? 'Pular' : 'Próxima'}
+      </Button>
+    </Col>
+
+    {((userRole === ROLE_VOTER && answersLength === minAnswers) || userRole === ROLE_CANDIDATE) && (
       <Col>
-        <Button
-          color="primary"
-          outline
-          block
-          type="button"
-          onClick={onBack}
-          disabled={questionIndex === 0}
-        >
-          Anterior
+        <Button type="submit" color="primary" block>
+          {userRole === ROLE_VOTER || (!alreadyAnswered && answersLength >= (minAnswers < 1)) ? 'Finalizar' : 'Responder'}
         </Button>
       </Col>
-
-      <Col>
-        <Button
-          color="primary"
-          outline
-          block
-          type="button"
-          onClick={() => onSkip()}
-          disabled={questionIndex === lastQuestionIndex}
-        >
-          {userRole === ROLE_CANDIDATE ? 'Pular' : 'Próxima'}
-        </Button>
-      </Col>
-
-      {userRole === ROLE_CANDIDATE && (
-        <Col>
-          <Button type="submit" color="primary" block>
-            {questionIndex === lastQuestionIndex && answersLength >= lastQuestionIndex ? 'Finalizar' : 'Responder'}
-          </Button>
-        </Col>
-      )}
-    </FormGroup>
-  );
-}
+    )}
+  </FormGroup>
+);
 
 export default QuestionnaireAction;
