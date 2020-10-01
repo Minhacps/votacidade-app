4c8dc1250225445876d6f6e9b7a9f7983a0be2ed
Victor Miguez <victorcineis@gmail.com>
2020-10-02 09:48:06 +1000

    Evita uma leitura adicional na collection de usuários

--- src/BaseAppRoutes.js
+++ src/BaseAppRoutes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Questionnaire from 'components/Questionnaire/Questionnaire';
 import HomePage from 'pages/Home';
@@ -8,28 +8,33 @@ import { ROLE_CANDIDATE } from 'constants/userRoles';
 import Ranking from 'pages/Ranking/Ranking';
 import Profile from 'pages/Profile/Profile';
 import MatchesProvider from 'components/MatchesProvider/MatchesProvider';
+import { CityContext } from 'components/CityProvider/CityProvider';
 
-const BaseAppRoutes = ({ cityPath, user, firebase }) => (
-  <Switch>
-    <Route path={cityPath} exact>
-      <HomePage />
-    </Route>
-    <Route path={`${cityPath}/questionario`} exact>
-      <Questionnaire user={user} />
-    </Route>
-    <MatchesProvider firebase={firebase}>
-      <Route path={`${cityPath}/ranking`} exact>
-        {user.role === ROLE_CANDIDATE ? (
-          <FinalPage user={user} />
-        ) : (
-          <Ranking user={user} />
-        )}
+const BaseAppRoutes = ({ user }) => {
+  const { firebase, cityPath } = useContext(CityContext);
+
+  return (
+    <Switch>
+      <Route path={cityPath} exact>
+        <HomePage />
       </Route>
-      <Route path={`${cityPath}/perfil/:candidateId`}>
-        <Profile />
+      <Route path={`${cityPath}/questionario`} exact>
+        <Questionnaire user={user} />
       </Route>
-    </MatchesProvider>
-  </Switch>
-);
+      <MatchesProvider firebase={firebase}>
+        <Route path={`${cityPath}/ranking`} exact>
+          {user.role === ROLE_CANDIDATE ? (
+            <FinalPage user={user} />
+          ) : (
+            <Ranking user={user} />
+          )}
+        </Route>
+        <Route path={`${cityPath}/perfil/:candidateId`}>
+          <Profile />
+        </Route>
+      </MatchesProvider>
+    </Switch>
+  );
+};
 
 export default BaseAppRoutes;
