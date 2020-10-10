487c2303b79fe3bdcf60291e42ad4a21cb9eb904
Luis Takahashi <takahashihideki408@gmail.com>
2020-10-10 13:47:57 -0300

    Fixed question test

--- src/components/Question/Question.test.js
+++ src/components/Question/Question.test.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import { render, screen, wait } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 
 import Question from './Question';
 
+import { ROLE_VOTER } from 'constants/userRoles';
+
 import CityProvider from 'components/CityProvider/CityProvider';
+import AnswersProvider from 'components/AnswersProvider/AnswersProvider';
+import questionsService from 'components/AnswersProvider/answersService';
+import { BrowserRouter } from 'react-router-dom';
+
+const storedAnswers = { 0: { answer: 'DT' }, 1: { answer: 'CT' } };
+beforeEach(() => {
+  jest
+    .spyOn(questionsService, 'getAnsweredQuestions')
+    .mockImplementation(() => Promise.resolve(storedAnswers));
+});
 
 jest.mock('components/CityProvider/CityProvider');
 
 describe('Question', () => {
   it('renders the provided question', async () => {
-    const mockSave = jest.fn();
+    const mockedUser = { role: ROLE_VOTER };
 
     render(
-      <CityProvider>
-        <Question id={1} onSave={mockSave} />
-      </CityProvider>,
+      <BrowserRouter>
+        <CityProvider>
+          <Question user={mockedUser} id={1} />
+        </CityProvider>
+      </BrowserRouter>,
+      {
+        wrapper: AnswersProvider,
+      },
     );
 
-    expect(screen.getByText('Any question title 1')).toBeVisible();
-    userEvent.click(screen.getByText('Discordo'));
-    userEvent.click(screen.getByText('Salvar'));
-    await wait(() => expect(mockSave).toBeCalled());
+    expect(await screen.findByText('Any question title 2')).toBeInTheDocument();
+
+    expect(screen.getByLabelText('Discordo')).toBeInTheDocument();
   });
 });
