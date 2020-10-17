f5ed6781ec3f1af67209399cdf2ae3735fb04299
Lucas Guima <lucasguima.felipe@gmail.com>
2020-10-17 16:02:34 -0300

    Update the Regex to validate the email accepting spaces before and after

--- src/components/User/SignUpForm.js
+++ src/components/User/SignUpForm.js
@@ -31,7 +31,7 @@ import { cidades } from 'data/cidades';
 import { AuthenticationContext } from '../../AuthenticationProvider';
 
 // eslint-disable-next-line no-useless-escape
-const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_REGEX = /^ *(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})) *$/;
 
 // eslint-disable-next-line no-useless-escape
 const CNPJ_REGEX = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
